9d62c1df8e55986f4831b8c07ce30cd84a51b57f
jrrlucenafilho <jrrlucenafilho@gmail.com>
2025-12-20 23:52:00 -0300

    add codecompanion to bufferline offset

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,22 +8,28 @@ return {
 			options = {
 				-- mode = "tabs",
 				offsets = {
-          {
-            filetype = "neo-tree",
-            text = "NeoTree",
-            text_align = "center",
-            separator = true,
-				  }
-        },
+					{
+						filetype = "neo-tree",
+						text = "NeoTree",
+						text_align = "center",
+						separator = true,
+					},
+					{
+						filetype = "codecompanion",
+						text = "Code Companion",
+						text_align = "center",
+						separator = true,
+					},
+				},
 			},
 		})
 
 		-- Bufferline keymaps
 		-- Go to the next buffer
-		vim.keymap.set("n", "<A-Right>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Cycle to next buffer"})
+		vim.keymap.set("n", "<A-Right>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Cycle to next buffer" })
 
 		-- Go to the previous buffer
-		vim.keymap.set("n", "<A-Left>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Cycle to previous buffer"})
+		vim.keymap.set("n", "<A-Left>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Cycle to previous buffer" })
 
 		-- Close current buffer
 		-- Disabled cause I'm using LazyVim's custom buf closing function
