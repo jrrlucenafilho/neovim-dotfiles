cc5d81d28036e7eb1ab1c98ca7b0b565eaf5242c
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-01-19 12:19:22 -0300

    rm fzf-native's own file

--- lua/plugins/telescope-fzf-native.lua
+++
@@ -1,5 +0,0 @@
--- Native telescope extension for fzf
-return {
-	"nvim-telescope/telescope-fzf-native.nvim",
-	build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install",
-}
