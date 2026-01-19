414309e78aba3deaed44a4ca91cb561b2f19aa22
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-01-19 17:41:34 -0300

    add outline-nvim

---
+++ lua/plugins/outline-nvim.lua
@@ -0,0 +1,12 @@
+-- Outline sidebar
+return {
+	"hedyhli/outline.nvim",
+	lazy = true,
+	cmd = { "Outline", "OutlineOpen" },
+	keys = { -- Example mapping to toggle outline
+		{ "<C-o>", "<cmd>Outline<CR>", desc = "Toggle outline" },
+	},
+	opts = {
+		-- Your setup opts here
+	},
+}
