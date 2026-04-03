6a56658b090db4d7b69d1ed75d3b1315233a978f
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-04-03 11:54:07 -0300

    feat(window-management): add nvim-window for quicker and smoother navigation

---
+++ lua/plugins/nvim-window.lua
@@ -0,0 +1,13 @@
+-- Quickly navigate between open windows
+return {
+	"yorickpeterse/nvim-window",
+	keys = {
+		{
+			mode = { "n", "i", "t" },
+			"<leader>w",
+			"<cmd>lua require('nvim-window').pick()<cr>",
+			desc = "Jump to window",
+		},
+	},
+	config = true,
+}
