dd583815c95c71a6d26df9601b90072f8bde058b
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-04-07 09:34:39 -0300

    feat(fidget-nvim):add translucency, border, and window avoidance to fidget-nvim

--- lua/plugins/fidget-nvim.lua
+++ lua/plugins/fidget-nvim.lua
@@ -93,8 +93,8 @@ return {
 				-- Options related to the notification window and buffer
 				window = {
 					normal_hl = "Comment", -- Base highlight group in the notification window
-					winblend = 100, -- Background color opacity in the notification window
-					border = "none", -- Border around the notification window
+					winblend = 25, -- Background color opacity in the notification window
+					border = "rounded", -- Border around the notification window
 					zindex = 45, -- Stacking priority of the notification window
 					max_width = 0, -- Maximum width of the notification window
 					max_height = 0, -- Maximum height of the notification window
@@ -103,7 +103,7 @@ return {
 					align = "bottom", -- How to align the notification window
 					relative = "editor", -- What the notification window position is relative to
 					tabstop = 8, -- Width of each tab character in the notification window
-					avoid = {}, -- Filetypes the notification window should avoid
+					avoid = { "codecompanion", "Outline" }, -- Filetypes the notification window should avoid
 					-- e.g., { "aerial", "NvimTree", "neotest-summary" }
 				},
 			},
