c25ac471e1d938b581279d4531f70d645d0a21f0
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-04-10 15:26:38 -0300

    tweak(context): change treesitter-context max_lines to 3

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -6,7 +6,7 @@ return {
 		require("treesitter-context").setup({
 			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
 			multiwindow = true, -- Enable multiwindow support.
-			max_lines = 2, -- How many lines the window should span. Values <= 0 mean no limit.
+			max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
 			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 			line_numbers = true,
 			multiline_threshold = 20, -- Maximum number of lines to show for a single context
@@ -19,7 +19,10 @@ return {
 			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 		})
 
-		----- [[ Keymaps ]] -----
+    ----- [[ Change highlight group ]] -----
+    vim.api.nvim_set_hl(0, 'TreesitterContext', { link = 'TelescopeSelection' })
+		
+    ----- [[ Keymaps ]] -----
 		-- Jump to context (top)
 		vim.keymap.set("n", "<A-c>", function()
 			require("treesitter-context").go_to_context(vim.v.count1)
