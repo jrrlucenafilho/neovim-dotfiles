71f676b74d02e719aae56a29a6f34b7a721efa79
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-04-25 08:21:43 -0300

    feat(marks): remove builtin marks

--- lua/plugins/marks-nvim.lua
+++ lua/plugins/marks-nvim.lua
@@ -8,7 +8,7 @@ return {
 			-- whether to map keybinds or not. default true
 			default_mappings = false,
 			-- which builtin marks to show. default {}
-			builtin_marks = { ".", "<", ">", "^" },
+			-- builtin_marks = { ".", "<", ">", "^" },
 			-- whether movements cycle back to the beginning/end of buffer. default true
 			cyclic = true,
 			-- whether the shada file is updated after modifying uppercase marks. default false
