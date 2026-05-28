8d72bb26f70d58ae09d57595ed2d9a1771003be8
jrrlucenafilho <jrrlucenafilho@gmail.com>
2026-05-28 13:50:00 -0300

    feat(trouble):change trouble config

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,38 +1,25 @@
 -- A pretty list for showing diagnostics, references, telescope results, quickfix and location lists
 return {
 	"folke/trouble.nvim",
-	opts = {},
 	cmd = "Trouble",
-	keys = {
-		{
-			"<leader>td",
-			"<cmd>Trouble diagnostics toggle<cr>",
-			desc = "Diagnostics (Trouble)",
-		},
-		{
+	config = function()
+		require("trouble").setup({})
+
+		vim.keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
+		vim.keymap.set(
+			"n",
 			"<leader>tf",
 			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Buffer Diagnostics (Trouble)",
-		},
-		{
-			"<leader>ts",
-			"<cmd>Trouble symbols toggle focus=false<cr>",
-			desc = "Symbols (Trouble)",
-		},
-		{
+			{ desc = "Buffer Diagnostics (Trouble)" }
+		)
+		vim.keymap.set("n", "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols (Trouble)" })
+		vim.keymap.set(
+			"n",
 			"<leader>tl",
 			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-			desc = "LSP Definitions / references / ... (Trouble)",
-		},
-		{
-			"<leader>tlo",
-			"<cmd>Trouble loclist toggle<cr>",
-			desc = "Location List (Trouble)",
-		},
-		{
-			"<leader>tq",
-			"<cmd>Trouble qflist toggle<cr>",
-			desc = "Quickfix List (Trouble)",
-		},
-	},
+			{ desc = "LSP Definitions / references / ... (Trouble)" }
+		)
+		vim.keymap.set("n", "<leader>tlo", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List (Trouble)" })
+		vim.keymap.set("n", "<leader>tq", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
+	end,
 }
